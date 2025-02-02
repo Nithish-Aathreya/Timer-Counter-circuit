092b70fed5a717844c81673dbe065bb94f6823f5
Nithish Aathreya <121860668+Nithish-Aathreya@users.noreply.github.com>
2025-02-02 21:20:18 +0530

    Update verify.v

--- verify.v
+++ verify.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: I am
 // 
-// Create Date: 26.10.2024 10:00:31
+// Create Date: 
 // Design Name: 
 // Module Name: verify
 // Project Name: 
@@ -20,31 +20,67 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module verify();
-reg clk,timer,timer_run,gate,intx;
-reg select;
-wire y;
+module verify;
+reg clk;
+reg reset;
+reg gate;
+reg intx;
+reg tr;
+reg cin;
+reg c_t;
+wire [15:0] count;
 
-proj uut(clk,timer,timer_run,gate,intx,y,select);
-
+main uut(clk,reset,gate,intx,tr,cin,c_t,count);
 initial
 begin
-$monitor("timer=%b,clk=%b,timer_run=%b,gate=%b,intx=%b,y=%b,select=%b",timer,clk,timer_run,gate,intx,y,select);
-timer = 0;
-forever
-#5 timer = ~timer; 
-end 
-
-initial
-begin
-clk = 0;
-forever
-#2 clk = ~clk;
+clk = 1'b0;
+reset =1'b1;
+gate =1'b0;
+intx = 1'b0;
+tr = 1'b0;
+cin = 1'b0;
+c_t = 1'b0;
 end
 
+
+always #41.6 clk = ~clk;
+
 initial
 begin
-   timer_run = 1; gate = 0; select = 0; intx = 0;
+ #50 reset = 0;
+        
+      
+        #20 tr = 1;
+        
+        
+        #500;
 
+        
+        #20 c_t = 1;  
+        tr = 1;
+        
+       
+        repeat (10) begin
+            #30 cin = 1'b1;
+            #30 cin = 1'b0;
+        end
+        
+       
+        #20 gate = 1;
+        intx = 0;    
+        #100 intx = 1; 
+        
+        
+        #500;
+        
+        
+        $stop;
 end
+
+   initial begin
+        $monitor("Time: %0t | c_t: %b | tr: %b | t_in: %b | gate: %b | int_in: %b | count: %b",
+                 $time, c_t, tr, cin, gate, intx, count);
+    end
+
 endmodule
+
